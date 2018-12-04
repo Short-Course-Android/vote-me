c82ee234af73bc77144f056aa444ad215ba6a6ea
cscdlab <codelbas.quabynah@gmail.com>
2018-12-04 10:34:57 +0000

    Home activity

--- vote-me-complete/vote-me-kotlin/app/src/main/java/io/ugshortcourse/voteme/view/HomeActivity.kt
+++ vote-me-complete/vote-me-kotlin/app/src/main/java/io/ugshortcourse/voteme/view/HomeActivity.kt
@@ -2,29 +2,32 @@ package io.ugshortcourse.voteme.view
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.ugshortcourse.voteme.R
 import io.ugshortcourse.voteme.core.VoteMeBaseActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity(override val layoutId: Int = R.layout.activity_home) : VoteMeBaseActivity() {
+    private lateinit var fm: FragmentManager
 
+    //Listener for change in click events for the bottom navigation view
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
-                message.setText(R.string.title_profile)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_feeds -> {
-                message.setText(R.string.title_feeds)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_elections -> {
-                message.setText(R.string.title_elections)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -32,7 +35,13 @@ class HomeActivity(override val layoutId: Int = R.layout.activity_home) : VoteMe
     }
 
     override fun onViewCreated(instanceState: Bundle?, intent: Intent) {
+        // Init fragment manager
+        fm = supportFragmentManager
+
+        //Add bottom navigation click listener
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
     }
 
 }
